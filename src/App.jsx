c3278d9b06e04a9feeadb7c696ee1035394a4612
Shreya Mehta <workwithshreyamehta@gmail.com>
2025-07-23 22:22:28 +0530

    Categories done.

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ import SingleProductsView from "./pages/SingleProductView";
 import About from "./pages/About";
 import Footer from "./components/Footer";
 import Fashion from "./pages/Fashion";
+import Decor from "./pages/Decor";
+import Pet from "./pages/Pet";
+import Beauty from "./pages/Beauty";
 
 function App() {
   return (
@@ -24,6 +27,9 @@ function App() {
           element={<SingleProductsView />}
         />
         <Route exact path="/fashion" element={<Fashion />} />
+        <Route exact path="/decor" element={<Decor />} />
+        <Route exact path="/pet" element={<Pet />} />
+        <Route exact path="/beauty" element={<Beauty />} />
       </Routes>
       <Footer />
     </div>
